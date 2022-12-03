1279fcea1f5a5078e897e5f742d6b1ea22e771f0
Thiago Castilho <tdecasti@my.centennialcollege.ca>
2022-12-02 19:19:45 -0500

    edit movie form completed

--- src/app/content/movie/edit/edit.component.ts
+++ src/app/content/movie/edit/edit.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MovieService } from 'src/app/services/movie.service';
 
 @Component({
   selector: 'app-edit',
@@ -7,9 +9,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieEditComponent implements OnInit {
 
-  constructor() { }
+  form: any = {
+    _id: null,
+    name: null,
+    year: null,
+    director: null,
+    genre: null,
+    runtime: null
+  }
+  
+  isSuccessfull = true;
+  errorMessage = "";
+
+  constructor(
+    private movieService: MovieService, 
+    private router: Router,
+    private route: ActivatedRoute
+    ) { }
 
   ngOnInit(): void {
+    this.route.params
+      .subscribe({
+        next: params => {
+          console.log(params);
+          this.form._id = params['id'];
+
+          this.movieService.getMovie(this.form._id)
+            .subscribe({
+              next: data => {
+                this.form = data.movie;
+              },
+              error: err => {
+                this.errorMessage = err.error.message;
+                this.isSuccessfull = false;
+              }
+            })
+
+        },
+        error: err => {
+          this.errorMessage = err.error.message;
+          this.isSuccessfull = false;
+        }
+      })
+  }
+
+  onSubmit(){
+    this.movieService.editMovie(this.form)
+      .subscribe({
+        next: data => {
+          console.log(data);
+          this.isSuccessfull = true;
+          this.backToList();
+        },
+        error: err => {
+          this.errorMessage = err.error.message;
+          this.isSuccessfull = false;
+        }
+      })
+  }
+  
+  backToList(){
+    this.router.navigate(['/movies/list']);
   }
 
 }
