371bf039818485de66c3d4ecedfc9b04d01be3f9
Thiago Castilho <tdecasti@my.centennialcollege.ca>
2022-12-02 19:09:04 -0500

    add movie form completed

--- src/app/content/movie/add/add.component.ts
+++ src/app/content/movie/add/add.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MovieService } from 'src/app/services/movie.service';
 
 @Component({
   selector: 'app-add',
@@ -7,9 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieAddComponent implements OnInit {
 
-  constructor() { }
+  form: any = {
+    name: null,
+    year: null,
+    director: null,
+    genre: null,
+    runtime: null
+  }
+
+  isSuccessfull = true;
+  errorMessage = "";
+
+  constructor(private movieService: MovieService, private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(){
+    this.movieService.addMovie(this.form)
+      .subscribe({
+        next: data => {
+          console.log(data);
+          this.isSuccessfull = true;
+          this.backToList();
+        },
+        error: err => {
+          this.errorMessage = err.error.message;
+          this.isSuccessfull = false;
+        }
+      })
+  }
+  
+  backToList(){
+    this.router.navigate(['/movies/list']);
   }
 
 }
